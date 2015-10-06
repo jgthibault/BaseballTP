75f4e6f8df802d34d1fabc65b11e53d3c259a4c0
jgthibault <jgabrielt@1-1186-VM.bergerweb.ltd>
2015-10-06 12:58:20 -0400

    Début des gestions d'erreur

--- php/Season.php
+++ php/Season.php
@@ -1,13 +1,14 @@
 <?php
 
-class Season 
+class Season extends BaseClass
 {
     public $Year;
     public $Name;
     public $IsCurrent;
-    
+	    
     function __construct()
-    {}
+    {
+	}
     
     public function initProperty($year, $name, $isCurrent)
     {   
@@ -27,7 +28,16 @@ class Season
         $this->Year = $year;
         $this->Name = $name;
         $this->IsCurrent = $isCurrent;
+		
+		$this->initValueState();
     }
+	
+	private function initValueState()
+	{
+		$ValueState = array("year" => "", 
+							"name" => "",
+							"isCurrent" => "");
+	}
     
     public function getIsCurrent()
     {
@@ -41,9 +51,23 @@ class Season
         }
     }
     
-    public function hasError()
+    public function validate()
     {
-        
+		$this->initValueState();
+		
+        if (!is_numeric($this->Year))
+		{
+			$ValueState["year"] = "L'année doit être un nombre valide";
+		}
+		
+		if (strlen
     }
+	
+	public function hasError()
+	{
+		return $ValueState["year"] <> "" or 
+			   $ValueState["name"] <> "" or
+			   $ValueState["isCurrent"] <> "" or;
+	}
 } 
 ?>
